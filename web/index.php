c4bf5344e8ce6a9866522bfffd227252bfc591a8
Hervé Tutuaku <herve.tutuaku@gmail.com>
2017-04-21 00:39:36 +0200

    [feature] add comments

--- web/index.php
+++ web/index.php
@@ -4,25 +4,17 @@ $loader = require_once __DIR__.'/../vendor/autoload.php';
 $loader->add('Pinnackl\Controller', realpath(__DIR__.'/..').'/src');
 $loader->add('Pinnackl\Helper', realpath(__DIR__.'/..').'/src');
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\ParameterBag;
-
+// Init application
 $app = new Silex\Application();
 
+// Dev purpose
 $app['debug'] = true;
 
 $helper = new Pinnackl\Helper\Config(realpath(__DIR__.'/..').'/config/config.php');
 
 $app['helper'] = $helper;
 
-// Convert application/json data to Array
-// $app->before(function (Request $request) {
-//     if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-//         $data = json_decode($request->getContent(), true);
-//         $request->request->replace(is_array($data) ? $data : array());
-//     }
-// });
-
+// Lazy instanciate controller class
 $toController = function($shortClass, $shortMethod)
 {
 	return sprintf('Pinnackl\Controller\%s::%sAction', $shortClass, $shortMethod);
@@ -36,6 +28,7 @@ $app->get('/apis', $toController('KilimanApi', 'list'));
 $app->get('/api/show/{id}', $toController('KilimanApi', 'show'));
 // $app->put('/api/update', $toController('KilimanApi', 'update'));
 // $app->delate('/api/delate', $toController('KilimanApi', 'delate'));
+
 /**
  * User API
  */
