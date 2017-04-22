6fc43f25ecaa04328cc6e0508a45ff223c09cfb3
Hervé Tutuaku <herve.tutuaku@gmail.com>
2017-04-22 17:53:01 +0200

    [feature] add API create method

--- src/Pinnackl/Controller/KilimanApi.php
+++ src/Pinnackl/Controller/KilimanApi.php
@@ -14,7 +14,35 @@ class KilimanApi
 {
     public function createAction(Request $request, \Silex\Application $app)
     {
-        return '';
+        // Get the helper
+        $helper = $app['helper'];
+
+        // Retreive the config
+        $baseApi = $helper->getConfig('baseApi');
+        $apiKey = $helper->getConfig('apiKey');
+        $adminWebToken = $helper->getConfig('adminWebToken');
+        $guzzleOpt = $helper->getConfig('guzzleOpt');
+
+        // Handle the POST request
+        $data = $request->getContent();
+
+        // Make the request to the Umbrella API
+        $client = new Client($guzzleOpt);
+
+        // Set headers
+        $res = $client->request('POST', $baseApi . '/apis.json', [
+            'headers'       => [
+                'content-type' => 'application/json',
+                'X-Api-Key' => $apiKey,
+                'X-Admin-Auth-Token' => $adminWebToken,
+            ],
+            'body'          => $data
+        ]);
+
+        // Send the Respnse
+        $response = new Response($res->getBody());
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
     public function listAction(Request $request, \Silex\Application $app)
